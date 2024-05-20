d7fccab9135f0a60dfacd508de4542fbe509a57d
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-05-20 16:55:36 -0300

    Criado o componente app.conversor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { ConversorComponent } from "./conversor/conversor.component"
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, ConversorComponent]
 })
 export class AppComponent {
   title = 'lo-conversor-de-moedas.app';
