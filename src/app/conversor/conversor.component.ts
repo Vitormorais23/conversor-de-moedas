3095e7834857752766dee3f8b80f6bc70820f8f9
José Vitor Nunes de Morais <josevitornm2014@outlook.com>
2024-05-22 17:45:18 -0300

    Obetendo moedas da API, e usando diretiva ngFor para exibir as Moedas

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conversor',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './conversor.component.html',
   styleUrl: './conversor.component.css'
 })
-export class ConversorComponent {
+export class ConversorComponent implements OnInit {
+
+  moedasList: {codigo: string, descricao: string}[] = []
+moedas: any;
+private _moeda: any;
+  
+  // Classe para consumir arquivos externos de http - uma API
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://economia.awesomeapi.com.br/json/available/uniq').subscribe((_moedas: any) => {
+      console.log(_moedas)
+      Object.keys(_moedas).forEach(key => {
+        this.moedasList.push({
+          codigo: key,
+          descricao: _moedas[key]
+        })
+      })
+      console.log(this.moedasList)
+    })
+  }
+
   calcular() {
     alert("Usando o Event Binding")
   }
