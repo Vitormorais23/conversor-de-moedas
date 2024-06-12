3bbec2aca0e391a76ae55481e65323f10d63de46
José Vitor - Casa <josevitornm2014@outlook.com>
2024-06-11 23:12:33 -0300

    Alternar o background por rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Route, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ConversorComponent } from "./components/conversor/conversor.component"
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+    pathUrl: string = 'conversor'
+
+    constructor(private router: Router) {
+
+    }
+
+    ngOnInit(): void {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.pathUrl = event.url.replace('/', '')
+            }
+        })
+    }
 }
