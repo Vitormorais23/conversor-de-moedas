8fad602755e5fbfc19fad95a6586b4f28add07dd
José Vitor - Casa <josevitornm2014@outlook.com>
2024-07-01 23:46:30 -0300

    Cotações com NgFor

--- src/app/components/cotacoes/cotacoes.component.ts
+++ src/app/components/cotacoes/cotacoes.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { AwesomeApiService } from '../../services/awesomeapi.service';
 import { Cotacao } from '../../models/cotacao.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cotacoes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cotacoes.component.html',
   styleUrl: './cotacoes.component.css'
 })
 export class CotacoesComponent implements OnInit {
+
+  cotacoes: Cotacao[] = []
 
   constructor(private apiService: AwesomeApiService) {
 
   }
 
   ngOnInit(): void {
-    this.apiService.getCotacoes().subscribe((cotacoes: Cotacao[]) => {
-      console.log(cotacoes)
+    this.apiService.getCotacoes().subscribe((cotacoe$: Cotacao[]) => {
+      this.cotacoes = cotacoe$
     })
   }
+
+
 }
--- src/app/models/cotacao.model.ts
+++ src/app/models/cotacao.model.ts
@@ -10,7 +10,7 @@ export class Cotacao {
     constructor(value?: any) {
         this.code = String(value?.code)
         this.codein = String(value?.codein)
-        this.name = String(value?.name)
+        this.name = String(value?.name?.substring(0, value?.name?.indexOf('/')))
         this.varBid = Number(value?.varBid)
         this.bid = Number(value?.bid)
         this.ask = Number(value?.ask)
@@ -19,5 +19,25 @@ export class Cotacao {
 
     getValor(): number {
         return (this.bid + this.ask) / 2
+    }
+
+    getVariacao(): string {
+        let prefix = ''
+        // if (typeof this.varBid === 'number' && this.varBid > 0) {
+        //     prefix = "+"
+        // }
+        // Forma de forçar o TypeScript ver que é um number
+        if ((this.varBid as number) > 0) {
+            prefix = '+'
+        }
+        return prefix + this.varBid?.toFixed(4)
+    }
+
+    getValorMonetario(): string {
+        return Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: String(this.codein),
+            maximumFractionDigits: 4
+          }).format(this.getValor())
     }
 }
